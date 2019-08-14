b54f46901408dc622f0cd57310aa1e18a74801cd
Álvaro Castro-Castilla <a@fourthbit.com>
2019-08-14 13:38:59 +0300

    Major update

--- blog/blog/zinnia/urls/search.py
+++
@@ -1,6 +0,0 @@
-"""Urls for the zinnia search"""
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('zinnia.views.search',
-                       url(r'^$', 'entry_search', name='zinnia_entry_search'),
-                       )
--- blog/blog/zinnia/urls/tests.py
+++
@@ -1,8 +0,0 @@
-"""Test urls for the zinnia project"""
-from django.conf.urls.defaults import *
-from zinnia.urls import urlpatterns
-
-urlpatterns += patterns('',
-                        url(r'^comments/', include('django.contrib.comments.urls')),
-                        )
-                        
